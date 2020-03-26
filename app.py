ec8917b7e700b5be98448cf2cefb0aaa93d148ba
Bhavneesh Sharma <54917282+bs3537@users.noreply.github.com>
2020-03-26 04:05:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,28 +26,32 @@ def create_app():
 #################APP ROUTES####################
 
     @app.route('/')
-    def home():
-        return render_template('index.html')
+    def welcome():
+        return "AirBNB Listing Price Prediction: API by Bhavneesh Sharma"
 
-    @app.route('/predict', methods=['POST', 'GET'])
+    @app.route('/predict', methods=['POST','GET'])
     def predict():
-        model = joblib.load(open('airbnb_model.sav', 'rb'))
-        int_features = [int(x) for x in request.form.values()]
-        final_features = [np.array(int_features)]
-        prediction = model.predict(final_features)
-        output = round(prediction[0])
-        #return render_template('index.html', prediction_text='The predicted home price is = $ {}'.format(output))
-        return jsonify(output)
-    
-    @app.route('/api', methods=['POST', 'GET'])
-    def prediction():
-        data = request.get_json(force=True)
-        model = joblib.load(open('airbnb_model.sav', 'rb'))
-        prediction = model.predict([[np.array(data['room_type'], ['neighbourhood_group'])]])
-        output = round(prediction[0])
-        return jsonify(output)
+  
+        if flask.request.method == 'GET':
+            return "Prediction page"
+ 
+        if flask.request.method == 'POST':
+            try:
+                json_ = request.json
+                print(json_)
+                query_ = pd.get_dummies(pd.DataFrame(json_))
+                query = query_.reindex(columns = model_columns, fill_value= 0)
+                prediction = list(model.predict(query))
+ 
+                return jsonify({
+                    "prediction":str(prediction)
+                })
+ 
+            except:
+                return jsonify({
+                    "trace": traceback.format_exc()
+                    })
+      
     return app
-
 if __name__ == "__main__":
-    model = joblib.load(open('airbnb_model.sav', 'rb'))
     app.run(debug=True)
